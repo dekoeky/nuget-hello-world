04ebf26423f753aef2e47e2c4f43c0a994fbcfa9
Wesley Van Koeckhoven <wvk@outlook.be>
2023-10-21 01:29:04 +0200

    use (outdated) nuget packages

--- dekoeky.hello-world/Greeting.cs
+++ dekoeky.hello-world/Greeting.cs
@@ -1,13 +1,23 @@
-﻿namespace dekoeky.hello_world;
+﻿using Serilog;
+
+namespace dekoeky.hello_world;
 
 public static class Greeting
 {
-  public static string Get()
-  {
-    return "Hello, World!" + Environment.NewLine + "Greetings, dekoeky";
-  }
-  public static void Perform()
-  {
-    Console.WriteLine(Get());
-  }
+    private static readonly ILogger Logger;
+
+    static Greeting()
+    {
+        Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
+    }
+    public static string Get()
+    {
+        return "Hello, World!" + Environment.NewLine + "Greetings, dekoeky";
+    }
+    public static void Perform()
+    {
+        Logger.Information(Get());
+    }
 }
