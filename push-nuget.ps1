f25b3711fc2c81a4aa26dc8968cdd1e211459c66
Wesley Van Koeckhoven <wvk@outlook.be>
2023-10-21 19:09:32 +0200

    get apikey from user secrets

--- push-nuget.ps1
+++ push-nuget.ps1
@@ -1,4 +1,16 @@
-dotnet nuget push .\dekoeky.hello-world\bin\Release\dekoeky.hello-world.*.nupkg `
+# This script can be used to manually push a NuGet package version to NuGet.org
+
+$version = '*' # The * symbol is a wildcard
+
+# Step 1: Retrieve the ApiKey from the projects user secrets
+# (This is done to prevent storing the key in the .git directory)
+
+$json = dotnet user-secrets list --json --project dekoeky.hello-world
+$secrets = $json | % { $_ -replace '//(BEGIN|END)' } | ConvertFrom-Json
+$apiKey = $secrets.'NuGet:Api-Keys:NuGet.org'
+
+# Step 2: Push the nuget package
+dotnet nuget push ".\dekoeky.hello-world\bin\Release\dekoeky.hello-world.$version.nupkg" `
   --skip-duplicate `
   -s https://api.nuget.org/v3/index.json .\check-nuget-problems.ps1 `
-  --api-key NUGET.ORG_API_KEY
+  --api-key $apiKey
