3c446f0ec8d02811649c24b8f4b4b5de7e6cfe80
Wesley Van Koeckhoven <wvk@outlook.be>
2023-10-21 02:16:14 +0200

    add tests

---
+++ dekoeky.hello-world.tests/GreetingTests.cs
@@ -0,0 +1,33 @@
+﻿namespace dekoeky.hello_world.tests;
+
+[TestClass]
+public class GreetingTests
+{
+    [TestMethod]
+    public void Get()
+    {
+        //Act
+        var greet = Greeting.Get();
+
+        //Assert
+        Assert.IsNotNull(greet);
+        Assert.IsFalse(string.IsNullOrEmpty(greet));
+        Assert.IsFalse(string.IsNullOrWhiteSpace(greet));
+    }
+
+    [TestMethod]
+    public void Perform()
+    {
+        var sw = new StringWriter();
+        Console.SetOut(sw);
+
+        //Act
+        Greeting.Perform();
+
+        //Assert
+        var greet = sw.ToString();
+        Assert.IsNotNull(greet);
+        Assert.IsFalse(string.IsNullOrEmpty(greet));
+        Assert.IsFalse(string.IsNullOrWhiteSpace(greet));
+    }
+}
